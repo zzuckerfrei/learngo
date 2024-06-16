72505d7e346b0b8d5f0456ffbd450ad9d4f6ddb9
zzuckerfrei <seonmin1219@gmail.com>
2024-06-16 21:57:11 +0900

    20240616

--- main.go
+++ main.go
@@ -1,47 +1,53 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/zzuckerfrei/learngo/mydict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("request failed")
+
+func hitUrl(url string) error {
+	fmt.Println("check :", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
+
 func main() {
-	dictionary := mydict.Dictionary{"first": "1"}
+	// initialize
+	// if you want to initialize an empty map, do like this.
+	// if not your map goes nil and you can't write into nil.
+	var results = make(map[string]string)
 
-	// search
-	definition, error := dictionary.Search("first")
-	if error == nil {
-		fmt.Println(definition)
-	} else {
-		fmt.Println(error)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	// add
-	word := "hello"
-	definition = "my friend"
-	error = dictionary.Add(word, definition)
-	if error != nil {
-		fmt.Println(error)
-	}
-	value, _ := dictionary.Search(word)
-	fmt.Println("found :", word, "definition : ", value)
-
-	// update
-	word = "first"
-	definition = "11"
-	error = dictionary.Update(word, definition)
-	if error == nil {
-		fmt.Println("update key :", word, "value :", dictionary[word])
-	} else {
-		fmt.Println(error)
+	// idx, val
+	for _, url := range urls {
+		result := "OK"
+		err := hitUrl(url)
+		if err != nil {
+			result = "Failed"
+		}
+		results[url] = result
 	}
 
-	// delete
-	word = "first"
-	error = dictionary.Delete(word)
-	if error != nil {
-		fmt.Println(error)
+	// key, val
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
-	fmt.Println(dictionary.Search(word))
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -2,14 +2,15 @@ package mydict
 
 import (
 	"errors"
+	"fmt"
 )
 
 // kind of alias
 type Dictionary map[string]string
 
 var (
-	errorNotFound     = errors.New("Not Found")
-	errorWordExists   = errors.New("That word already exists")
+	errorNotFound     = errors.New("not found")
+	errorWordExists   = errors.New("that word already exists")
 	errorCannotUpdate = errors.New("can't update")
 	errosCannotDelete = errors.New("cannot delete")
 )
@@ -66,4 +67,44 @@ func (d Dictionary) Delete(word string) error {
 	}
 	delete(d, word)
 	return nil
+}
+
+func main() {
+	dictionary := mydict.Dictionary{"first": "1"}
+
+	// search
+	definition, error := dictionary.Search("first")
+	if error == nil {
+		fmt.Println(definition)
+	} else {
+		fmt.Println(error)
+	}
+
+	// add
+	word := "hello"
+	definition = "my friend"
+	error = dictionary.Add(word, definition)
+	if error != nil {
+		fmt.Println(error)
+	}
+	value, _ := dictionary.Search(word)
+	fmt.Println("found :", word, "definition : ", value)
+
+	// update
+	word = "first"
+	definition = "11"
+	error = dictionary.Update(word, definition)
+	if error == nil {
+		fmt.Println("update key :", word, "value :", dictionary[word])
+	} else {
+		fmt.Println(error)
+	}
+
+	// delete
+	word = "first"
+	error = dictionary.Delete(word)
+	if error != nil {
+		fmt.Println(error)
+	}
+	fmt.Println(dictionary.Search(word))
 }
